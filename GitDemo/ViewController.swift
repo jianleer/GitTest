22d3d1623c4444c55d6c154708829f5e3e242061
jianleer <1449190335@qq.com>
2015-03-30 10:16:09 +0800

    初始化导入

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let a  = 4
+        let  b = 23
+        
     }
 
     override func didReceiveMemoryWarning() {
